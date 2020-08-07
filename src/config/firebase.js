1074ef30a2bb6f184cf0e35a6fb980644afbaf7f
Dan Percic <danpercic86@gmail.com>
2020-08-07 19:23:31 +0300

    add dsc upt firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,25 +1,28 @@
 import firebase from 'firebase/app';
-import 'firebase/auth';   
+import 'firebase/auth';
 import 'firebase/storage';
 import 'firebase/firestore';
 import 'firebase/messaging';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOl0qzkhCDmyWeeIT8AZe4xyJJlIfU5gE",
-  authDomain: "myaurapp.firebaseapp.com",
-  databaseURL: "https://myaurapp.firebaseio.com",
-  projectId: "myaurapp",
-  storageBucket: "myaurapp.appspot.com",
-  messagingSenderId: "419511997060",
-  appId: "1:419511997060:web:ce7aa0d308f6b484f868d1"
+    apiKey: "AIzaSyApB04_-2R3jflCtAL4-9T_DO_qE5ngT4k",
+    authDomain: "dsc-upt.firebaseapp.com",
+    databaseURL: "https://dsc-upt.firebaseio.com",
+    projectId: "dsc-upt",
+    storageBucket: "dsc-upt.appspot.com",
+    messagingSenderId: "1004294201218",
+    appId: "1:1004294201218:web:d46e92c65cb171d756399b",
+    measurementId: "G-CDKMWCKB9J"
 };
 
 firebase.initializeApp(firebaseConfig);
-firebase.firestore().enablePersistence({synchronizeTabs:!0}).catch(()=>{console.warn("DB offline support not available")})
-export default{
-  notificationSupported:firebase.messaging.isSupported(),
-  messaging: (firebase.messaging.isSupported())?firebase.messaging():null,
-  firestore: firebase.firestore(),
-  auth:firebase.auth(),
-  storage:firebase.storage(),
+firebase.firestore().enablePersistence({synchronizeTabs: !0}).catch(() => {
+    console.warn("DB offline support not available")
+})
+export default {
+    notificationSupported: firebase.messaging.isSupported(),
+    messaging: (firebase.messaging.isSupported()) ? firebase.messaging() : null,
+    firestore: firebase.firestore(),
+    auth: firebase.auth(),
+    storage: firebase.storage(),
 };
